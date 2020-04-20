6f055b550bac246ca888b42527662c62e0df1f74
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-04-20 15:23:59 -0500

    Feat: added js.

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready( function() {
+    "use strict";
+
+    $(window).on('load', function () {
+
+        $('.hello').animate({
+            left: '345px',
+            opacity: '1'
+        }, 1500);
+    });
+
+
+
+});
