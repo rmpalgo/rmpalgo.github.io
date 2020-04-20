8d4116929e5bd82ad5a45d9c3d669412ac73b587
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-04-20 16:52:55 -0500

    Feat: displayed hide for about line and pushed hello to side when scroll up.

--- js/main.js
+++ js/main.js
@@ -11,6 +11,15 @@ $(document).ready( function() {
 
     $(window).scroll(function () {
        console.log('scrolled');
+       $('.hello').animate({
+           left: "275px"
+       }, 1000);
+        $('.dot').css('display', 'none');
+        $('.dot').next().css('display', 'none');
+        $('.about-word').fadeOut();
+        $('.about-word').prev().fadeOut();
+        $('.hello').children().children().show();
+
     });
 
 });
